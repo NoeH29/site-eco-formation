30e9d935b735d8bd792324c5427921f56debd72e
Youri Martin <youri.martin@gmail.com>
2021-03-03 09:18:40 +0100

    'debut'

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import Axios from "axios";
+import App from "./App.vue";
+import router from "./router";
+import session from "vue-session";
+import store from './store'
 
-Vue.config.productionTip = false
+Vue.use(session);
+Vue.config.productionTip = false;
+Vue.prototype.http = Axios;
+
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  store,
+  render: (h) => h(App)
+}).$mount("#app");
---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>{{ $store.state.auteur }}</h1>
+    <h2>{{ $store.state.bigcount }}</h2>
+    <button v-on:click="addbig()">big</button>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    addbig() {
+      this.$store.dispatch("ajoute1");
+    },
+  },
+};
+</script>
