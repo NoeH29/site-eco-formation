de7257e907fd59a67c978b1b79f5eeb878e50e9a
Youri <you.ludo.noe.kev@gmail.com>
2021-03-09 10:26:01 +0100

    testyouri

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,10 @@
 <template>
-  <div class="home">
-    <h1>youri</h1>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div class="home"></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
 <style scoped  >
