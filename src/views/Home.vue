fa3fcad95816f97e3d0242b006f67824bfb7c07b
NoeH <74050433+Pesosito@users.noreply.github.com>
2021-03-09 10:26:15 +0100

    footer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,74 @@
 <template>
   <div class="home">
-    <h1>youri</h1>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <section id="page">
+      <header>
+
+      </header>
+
+      <nav>
+
+      </nav>
+
+      <main>
+
+      </main>
+
+      <footer>
+        <h1>Youri , Noé , Ludo , Kevin</h1>
+        <Footer/>
+      </footer>
+
+    </section>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Footer from "@/components/Footer.vue";
+
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    Footer
+,
   },
 };
 </script>
 <style scoped  >
+#page {
+  display: grid;
+  width:100%;
+  height: 875px;
+  grid-template-areas: "head head head"
+                       "nav nav nav"
+                       "main main main"
+                       "foot foot foot";
+
+
+}
+
+#page > header {
+  grid-area: head;
+  background-color: #9e2c2c;
+}
+
+#page > nav {
+  grid-area: nav;
+  background-color: #3937ac;
+}
+
+#page > main {
+  grid-area: main;
+  background-color: #53bd45;
+}
+
+#page > footer {
+  height: 100%;
+  grid-area: foot;
+  background-color: #c8d86c;
+  display:flex;
+  flex-direction: column;
+  justify-content: end;
+}
 </style>
